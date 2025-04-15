428ec17a90b74e05989afc27f346db9b8d7d6326
Pranav Shinde <95024774+Pranavshinde678@users.noreply.github.com>
2025-04-15 22:33:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-# Importing modules
 import numpy as np
 import streamlit as st
 import cv2
 import pandas as pd
-
 from collections import Counter
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
 import base64
-
 
 
 df = pd.read_csv("muse_v3.csv")
@@ -43,7 +40,7 @@ def fun(list):
         if v == 'Neutral':
             data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
         elif v == 'Angry':
-             data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
+            data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
         elif v == 'fear':
             data = pd.concat([data, df_fear.sample(n=t)], ignore_index=True)
         elif v == 'happy':
@@ -73,7 +70,7 @@ def fun(list):
             v = list[i]          
             t = times[i]
 
-            if v == 'Neutral':              
+            if v == 'Neutral':               
                 data = pd.concat([data, df_neutral.sample(n=t)], ignore_index=True)
             elif v == 'Angry':               
                 data = pd.concat([data, df_angry.sample(n=t)], ignore_index=True)
@@ -127,9 +124,6 @@ def pre(l):
         result.extend([emotion] * count)
     print("Processed Emotions:", result)
 
-    # result = [item for items, c in Counter(l).most_common()
-    #           for item in [items] * c]
-
     ul = []
     for x in result:
         if x not in ul:
@@ -137,17 +131,12 @@ def pre(l):
             print(result)
     print("Return the list of unique emotions in the order of occurrence frequency :",ul)
     return ul
-    
 
-
-
-
+# Load model
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
@@ -156,25 +145,10 @@ model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.5))
-
 model.add(Dense(7, activation='softmax'))
-
-
 model.load_weights('model.h5')
 
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
-
-
-cv2.ocl.setUseOpenCL(False)
-cap = cv2.VideoCapture(0) 
-
-print("Loading Haarcascade Classifier...")
-face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-if face.empty():
-    print("Haarcascade Classifier failed to load.")
-else:
-    print("Haarcascade Classifier loaded successfully.")
 
 page_bg_img = '''
 <style>
@@ -201,40 +175,26 @@ with col2:
         count = 0
         list.clear()
 
-        while True:
+        uploaded_image = st.camera_input("Capture emotion")
 
-            ret, frame = cap.read()
-            if not ret:
-                break
-            
-            face = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")          
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)        
-            faces = face.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)          
-            count = count + 1
+        if uploaded_image:
+            img = uploaded_image.get_image()
 
+            # Convert the image to numpy array
+            img_array = np.array(img)
+
+            gray = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+            faces = face.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
             for (x, y, w, h) in faces:               
-                cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)               
-                roi_gray = gray[y:y + h, x:x + w]               
-                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)             
-                prediction = model.predict(cropped_img)             
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = model.predict(cropped_img)              
                 max_index = int(np.argmax(prediction))
 
                 list.append(emotion_dict[max_index])
 
-                cv2.putText(frame, emotion_dict[max_index], (x + 20, y - 60), 
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)              
-                cv2.imshow('Video', cv2.resize(frame, (1000, 700), interpolation=cv2.INTER_CUBIC))
-
-            if cv2.waitKey(1) & 0xFF == ord('s'):
-                break
-            if count >= 20:
-                break
-        cap.release()
-        cv2.destroyAllWindows()
-
         list = pre(list)
-        st.success(" emotions successfully detected") 
-        
+        st.success("Emotions successfully detected")
 
 with col3:
     pass
@@ -248,11 +208,9 @@ st.markdown("<h5 style='text-align: center; color: grey;'><b>Recommended song's 
 st.write("---------------------------------------------------------------------------------------------------------------------")
 
 try:
-  
-    for l,a,n,i in zip(new_df["link"],new_df['artist'],new_df['name'],range(30)):
-
+    for l, a, n, i in zip(new_df["link"], new_df['artist'], new_df['name'], range(30)):
         st.markdown("""<h4 style='text-align: center;'><a href={}>{} - {}</a></h4>"""
-                    .format(l,i+1,n),unsafe_allow_html=True)
+                    .format(l, i+1, n), unsafe_allow_html=True)
         st.markdown("<h5 style='text-align: center; color: grey;'><i>{}</i></h5>" 
                     .format(a), unsafe_allow_html=True)
         st.write("---------------------------------------------------------------------------------------------------------------------")
