cd761bb5448636292739f975468a521814360f1c
Pranav Shinde <95024774+Pranavshinde678@users.noreply.github.com>
2025-04-15 21:46:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,6 @@ model.load_weights('model.h5')
 emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 
-cv2.ocl.setUseOpenCL(False)
 
 # Attempt to open the default camera (index 0)
 cap = cv2.VideoCapture(0)
